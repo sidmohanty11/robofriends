0853a6d1941d3d2bb93d188193227a2106f923ad
sidmohanty11 <sidmohanty11@gmail.com>
2021-06-27 16:03:40 +0530

    minor changes

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Card({username, name, email, id}) {
 	return(
 		<div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-			<img alt="" src={`https://robohash.org/${id}?200x200`} />
+			<img alt="" src={`https://robohash.org/${id}?size=250x250`} />
 			<div>
 				<h2>{username}</h2>
 				<p>name: {name}</p><p>email: {email}</p>
